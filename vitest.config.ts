3c868b8f8f2a280024a738a0012fbd8c2faf8f06
Jeshua Maxey <me@jeshua.co>
2025-12-15 22:17:14 +0000

    test: update vitest config to exclude old __tests__ directory
    - Add exclude pattern for __tests__/ directory
    - Tests will now only run from tests/ directory

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
     globals: true,
     environment: 'node',
     setupFiles: ['./tests/setup.ts'],
+    exclude: ['**/node_modules/**', '**/dist/**', '__tests__/**'],
   },
   resolve: {
     alias: {
